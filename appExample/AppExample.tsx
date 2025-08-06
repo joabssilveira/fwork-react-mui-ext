287ca9f72f0170c3979371c82a6dd75afbb929a6
joabssilveira <joabssilveira@gmail.com>
2025-08-06 17:34:32 -0300

    feat:app example

--- appExample/AppExample.tsx
+++ appExample/AppExample.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react"
-import { AutocompleteClientComponent } from '@lib/autocompleteClient'
+import { AutocompleteClientComponentExample } from "./autoCompleteClient"
 
 interface IPost {
   "userId": number,
@@ -9,38 +8,7 @@ interface IPost {
 }
 
 export const AppExample = () => {
-  const [post, setPost] = useState<IPost | null | undefined>(null)
-
-  async function fetchPosts(): Promise<IPost[]> {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
-    if (!response.ok) {
-      throw new Error('Erro ao buscar posts')
-    }
-    const data: IPost[] = await response.json()
-    return data
-  }
-
-  return <div style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
-    {post != null && <label>{`selected post: ${post?.title}`}</label>}
-    <AutocompleteClientComponent
-      getOnInit
-      getOptionLabel={(post: IPost) => {
-        return `${post.title} (${post.id})`
-      }}
-      style={{ width: 200 }}
-
-      inputValueKeyName="title"
-      // initKeyValue={undefined}
-      onChangeItem={(item?: IPost | null) => {
-        setPost(item)
-      }}
-      onGetData={async (filter: string, key: boolean) => {
-        return await fetchPosts()
-      }}
-      textFieldProps={{
-        label: 'Posts',
-        placeholder: 'Digite para consultar ou pressione enter para exibir tudo'
-      }}
-    />
+  return <div style={{ padding: 20, fontFamily: '"Roboto", "Helvetica", "Arial", "sans-serif"' }}>
+    <AutocompleteClientComponentExample />
   </div>
 }
---
+++ appExample/autoCompleteClient.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react"
+import { AutocompleteClientComponent } from '@lib/autocompleteClient'
+
+interface IPost {
+  "userId": number,
+  "id": number,
+  "title": string,
+  "body": string,
+}
+
+export const AutocompleteClientComponentExample = () => {
+  const [post, setPost] = useState<IPost | null | undefined>(null)
+
+  async function fetchPosts(): Promise<IPost[]> {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    if (!response.ok) {
+      throw new Error('Erro ao buscar posts')
+    }
+    const data: IPost[] = await response.json()
+    return data
+  }
+
+  return <div style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
+    <h2>AutocompleteClientComponent</h2>
+    {post != null && <label>{`selected post: ${post?.title}`}</label>}
+    <AutocompleteClientComponent
+      getOnInit
+      getOptionLabel={(post: IPost) => {
+        return `${post.title} (${post.id})`
+      }}
+      style={{ width: 200 }}
+
+      inputValueKeyName="title"
+      // initKeyValue={undefined}
+      onChangeItem={(item?: IPost | null) => {
+        setPost(item)
+      }}
+      onGetData={async (filter: string, key: boolean) => {
+        return await fetchPosts()
+      }}
+      textFieldProps={{
+        label: 'Posts',
+        placeholder: 'Digite para consultar ou pressione enter para exibir tudo'
+      }}
+    />
+  </div>
+}
